cf37c595e2b475c5bf364ce828e1155eb24c1644
Austin Burrow <atburrow@gmail.com>
2012-03-01 00:09:48 -0600

    Hyphens and underscores are now allowed in Slugs before processing.

--- system/utilities.php
+++ system/utilities.php
@@ -2,7 +2,7 @@
 function Slug($name)
 {
     $sanitized = strtolower(trim($name));
-    $sanitized = preg_replace('/[^a-z0-9 \t]/i', '', $sanitized);
+    $sanitized = preg_replace('/[^-_a-z0-9 \t]/i', '', $sanitized);
     return preg_replace('/[ \t]+/', '-', $sanitized);
 }
 ?>
