922c8d72f12035b3934a1a51cd64c51962e89c65
Austin Burrow <atburrow@gmail.com>
2012-02-29 23:44:25 -0600

    ControllerMain now generates a menu. It also handles page slugs and the default page.

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -2,5 +2,45 @@
 class ControllerMain extends ControllerTemplate
 {
     public $template = 'main';
+    
+    public function Before()
+    {
+        parent::Before();
+        
+        // Generate menus for the page.
+        $this->template->Variable('menu_items', 
+                Database::$current
+                    ->Query("SELECT * FROM `cms_menu` JOIN `cms_pages` "
+                        . "ON `cms_menu`.`page_id`=`cms_pages`.`page_id` "
+                        . "ORDER BY `cms_menu`.`position` ASC;")
+                    ->FetchArray());
+    }
+    
+    public function ActionIndex()
+    {
+        $page = Database::$current
+                    ->Query("SELECT * FROM `cms_pages` WHERE `default`=1")
+                    ->Fetch();
+        
+        $this->template->Variables(array(
+            'page_title' => $page['title'], 
+            'content' => $page['content'],
+            ));
+    }
+    
+    public function ActionPage()
+    {
+        // Select page based on slug name.
+        $page = Database::$current
+                    ->Query("SELECT * FROM `cms_pages` WHERE `slug`='"
+                        . $this->request->parameter('slug') . "' LIMIT 1")
+                    ->Fetch();
+        
+        // Add template information based on page.
+        $this->template->Variables(array(
+            'page_title' => $page['title'], 
+            'content' => $page['content'],
+            ));
+    }
 }
 ?>
