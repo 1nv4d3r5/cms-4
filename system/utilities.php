c65ba276a5c2454c5f0d1a1a1b4f505bdeac9723
Austin Burrow <atburrow@gmail.com>
2012-02-29 23:23:22 -0600

    Fixed Slug function in utilities. It now generates a correct slug.

--- system/utilities.php
+++ system/utilities.php
@@ -2,6 +2,6 @@
 
 function Slug($name)
 {
-    return implode(explode(strtolower($name), ' '), '-');
+    return implode('-', explode(' ', strtolower(preg_replace('/[^a-z0-9 ]/i', '', $name))));
 }
 ?>
