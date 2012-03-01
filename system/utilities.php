3caf1d835041937f6b44572835b6c996e6965ee1
Austin Burrow <atburrow@gmail.com>
2012-03-01 00:07:40 -0600

    Slug now generates one hyphen for multiple spaces or tabs between two words.

--- system/utilities.php
+++ system/utilities.php
@@ -1,7 +1,8 @@
 <?php
-
 function Slug($name)
 {
-    return implode('-', explode(' ', strtolower(preg_replace('/[^a-z0-9 ]/i', '', $name))));
+    $sanitized = strtolower(trim($name));
+    $sanitized = preg_replace('/[^a-z0-9 \t]/i', '', $sanitized);
+    return preg_replace('/[ \t]+/', '-', $sanitized);
 }
 ?>
