683a10dc3b756408720794fdf9f1db2f0e7dba1d
Austin Burrow <atburrow@gmail.com>
2012-03-03 17:45:23 -0600

    Added Admin::ActionAuthError for handling auth errors.

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -10,6 +10,11 @@ class ControllerAdmin extends Controller
         
         $this->auth = Auth::Factory();
         $this->user = $this->auth->user();
+        
+        if (!$this->user && !preg_match(',^' . CMS::base_url()
+            . 'admin/auth(/(.*))?$,', $this->request->uri()))
+            $this->request->Redirect('admin/auth');
+            
     }
     
     public function ActionIndex()
@@ -28,6 +33,31 @@ class ControllerAdmin extends Controller
         $this->response->body(View::Factory('admin/auth', array(
                 'page_title' => 'Authentication',
                 'content' => 'You must authenticate before proceeding.',
+            )));
+    }
+    
+    public function ActionAuthLogin()
+    {
+        $this->request->Redirect('admin/auth/error/failed');
+    }
+    
+    public function ActionAuthError()
+    {
+        $page_status = 'Unknown error. Please try again later.';
+        switch ($this->request->parameter('error_status'))
+        {
+            case 'failed':
+                $page_status = 'Username or password is incorrect. Please try again.';
+                break;
+            
+            case 'access':
+                $page_status = 'You must be authenticated to proceed.';
+                break;
+        }
+        
+        $this->response->body(View::Factory('admin/auth', array(
+                'page_title' => 'Authentication',
+                'page_status' => $page_status,
             )));
     }
 }
