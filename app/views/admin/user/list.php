16d7b9b902670898c7ac42428db28b59bef1e4e6
Austin Burrow <atburrow@gmail.com>
2012-03-03 23:52:27 -0600

    Added a case for user/list when there are no users. Although that should never happen, it's best to stay safe.

--- app/views/admin/user/list.php
+++ app/views/admin/user/list.php
@@ -1,14 +1,18 @@
 <?php if (!defined('INDIRECT')) die(); ?>
-<table>
-    <?php foreach ($users as $user): ?>
-    <tr>
-        <td><?php echo $user['user_id']; ?></td>
-        <td>
-            <a href="<?php echo URL::Absolute('admin/user/edit/' . $user['user_id']);?>">
-                <?php echo $user['username']; ?>
-            </a>
-        </td>
-        <td><a href="<?php echo URL::Absolute('admin/user/delete/' . $user['user_id']); ?>">Delete</a></td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+<?php if (count($users) > 0): ?>
+    <table>
+        <?php foreach ($users as $user): ?>
+        <tr>
+            <td><?php echo $user['user_id']; ?></td>
+            <td>
+                <a href="<?php echo URL::Absolute('admin/user/edit/' . $user['user_id']);?>">
+                    <?php echo $user['username']; ?>
+                </a>
+            </td>
+            <td><a href="<?php echo URL::Absolute('admin/user/delete/' . $user['user_id']); ?>">Delete</a></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+<?php else: ?>
+    <h3>No users.</h3>
+<?php endif; ?>
