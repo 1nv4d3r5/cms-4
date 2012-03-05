0ddf4a6329b547f2750cc557c7d6173ea1fb2913
Austin Burrow <atburrow@gmail.com>
2012-03-04 18:59:03 -0600

    Removed the table from the admin/user/list view.

--- app/views/admin/user/list.php
+++ app/views/admin/user/list.php
@@ -3,45 +3,33 @@
     <h3><?php echo $status_message; ?></h3>
 <?php endif; ?>
 <?php if (count($users) > 0): ?>
-    <table style="width: 100%; margin-top: 15px;">
-        <!--tr>
-            <td><input type="checkbox"/></td>
-            <td style="font-weight: bold; font-size: 1.4em;">Username</td>
-            <td style="font-weight: bold; font-size: 1.4em;">Actions</td>
-        </tr-->
+    <ul class="admin-user-list">
         <?php foreach ($users as $user): ?>
-        <tr>
-            <!--td><input type="checkbox"/></td-->
-            <td>
-                <a href="<?php echo URL::Absolute('admin/user/edit/' . $user['user_id']);?>"
-                    style="font-size: 1.4em; font-weight: bold;">
-                    <?php echo $user['username']; ?>
-                </a>
-            </td>
-            <td>
-                <a href="<?php echo URL::Absolute('admin/user/edit/' . $user['user_id']);?>"
-                    class="button" style="float: left; width: 80px;">
-                    <img src="<?php echo URL::Absolute('media/img/user-edit.png'); ?>"/>
-                    <div>Edit</div>
-                </a>
-                <a href="<?php echo URL::Absolute('admin/user/delete/' . $user['user_id']);?>"
-                    class="button" style="float: left; width: 100px;">
-                    <img src="<?php echo URL::Absolute('media/img/user-delete.png'); ?>"/>
-                    <div>Delete</div>
-                </a>
-            </td>
-        </tr>
+            <li>
+                <div style="width: 100px; overflow-x: hidden; display: inline-block; vertical-align: top; padding-top: 4px;">
+                    <a href="<?php echo URL::Absolute('admin/user/edit/' . $user['user_id']);?>"
+                        style="font-size: 1.4em; font-weight: bold; vertical-align: middle;"><?php echo $user['username']; ?></a>
+                </div>
+                <div style="display: inline-block;">
+                    <a href="<?php echo URL::Absolute('admin/user/edit/' . $user['user_id']);?>"
+                        class="button" style="float: left; width: 80px;">
+                        <img src="<?php echo URL::Absolute('media/img/user-edit.png'); ?>"/>
+                        <div>Edit</div>
+                    </a>
+                    <a href="<?php echo URL::Absolute('admin/user/delete/' . $user['user_id']);?>"
+                        class="button" style="float: left; width: 100px;">
+                        <img src="<?php echo URL::Absolute('media/img/user-delete.png'); ?>"/>
+                        <div>Delete</div>
+                    </a>
+                    <a href="<?php echo URL::Absolute('admin/user/arcive/' . $user['user_id']);?>"
+                        class="button" style="float: left; width: 100px;">
+                        <img src="<?php echo URL::Absolute('media/img/user-archive.png'); ?>"/>
+                        <div>Archive</div>
+                    </a>
+                </div>
+            </li>
         <?php endforeach; ?>
-        <!--tr>
-            <td colspan="3">
-                <div style="font-weight: bold; font-size: 1.1em; float: left; margin: 7px 7px 0 0;">With selected</div>
-                <a href="javascript:void(0);" class="button" style="float: left; width: 100px;">
-                    <img src="<?php echo URL::Absolute('media/img/user-delete.png'); ?>"/>
-                    <div>Delete</div>
-                </a>
-            </td>
-        </tr-->
-    </table>
+    </ul>
 <?php else: ?>
     <h3>No users.</h3>
 <?php endif; ?>
