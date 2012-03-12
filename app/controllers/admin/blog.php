af44cfd4207ee1217b446d34597bfee4e8cf0644
Austin Burrow <atburrow@gmail.com>
2012-03-12 13:37:56 -0500

    admin/blog/list section now correctly shows blog entries.

--- app/controllers/admin/blog.php
+++ app/controllers/admin/blog.php
@@ -11,9 +11,16 @@ class ControllerAdminBlog extends ControllerAdmin
     
     public function ActionList()
     {
+        $list_view = View::Factory('admin/blog/list', array(
+                'user'         => $this->user,
+                'blog_entries' => Database::current()
+                                      ->Query('SELECT * FROM `cms_blog_entries`')
+                                      ->FetchArray(),
+            ));
+        
         $this->template->Variables(array(
-                'page_title' => 'Manage Blog',
-                'content' => 'Blog list.',
+                'page_title' => 'Manage Blog Entries',
+                'content' => $list_view,
                 'user' => $this->user,
             ));
     }
--- app/views/admin/blog/list.php
+++ app/views/admin/blog/list.php
@@ -2,29 +2,25 @@
 <?php if (isset($status_message)): ?>
     <h3><?php echo $status_message; ?></h3>
 <?php endif; ?>
-<?php if (isset($entries) && count($entries) > 0): ?>
+<?php if (isset($blog_entries) && count($blog_entries) > 0): ?>
     <ul class="admin-user-list">
-        <?php foreach ($entries as $list_entry): ?>
+        <?php foreach ($blog_entries as $list_entry): ?>
             <li>
                 <div style="width: 320px; overflow-x: hidden; display: inline-block; vertical-align: top; padding-top: 4px;">
                     <a href="<?php echo URL::Absolute('admin/blog/edit/' . $list_entry['blog_entry_id']);?>"
                         style="font-size: 1.4em; font-weight: bold; vertical-align: middle;">
-                        <?php if ($list_entry['default']): ?>
-                            <img src="<?php echo URL::Absolute('media/img/page-default.png'); ?>"
-                                 style="width: 22px; height: 22px;"/>
-                        <?php endif; ?>
                         <?php echo $list_entry['title']; ?>
                     </a>
                 </div>
                 <div style="display: inline-block;">
-                    <?php if ($list_entry['editable']): ?>
+                    <?php if (isset($list_entry['editable']) && $list_entry['editable']): ?>
                         <a href="<?php echo URL::Absolute('admin/blog/edit/' . $list_entry['blog_entry_id']);?>"
                             class="button" style="float: left; width: 80px;">
                             <img src="<?php echo URL::Absolute('media/img/page-edit.png'); ?>"/>
                             <div>Edit</div>
                         </a>
                     <?php endif; ?>
-                    <?php if ($list_entry['published']): ?>
+                    <?php if (isset($list_entry['published']) && $list_entry['published']): ?>
                         <a href="<?php echo URL::Absolute('admin/blog/unpublish/' . $list_entry['blog_entry_id']);?>"
                             class="button" style="float: left; width: 115px;">
                             <img src="<?php echo URL::Absolute('media/img/page-unpublish.png'); ?>"/>
@@ -47,7 +43,7 @@
         <?php endforeach; ?>
     </ul>
 <?php else: ?>
-    <h3>No pages.</h3>
+    <h3>No blog entries.</h3>
 <?php endif; ?>
 <?php if ($user['permission_pages_add']): ?>
     <div class="admin-section-menu">
