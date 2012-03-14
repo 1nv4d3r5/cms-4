200ec3cac8bb53f5c818704a3dcfbb65d537212f
Austin Burrow <atburrow@gmail.com>
2012-03-13 23:12:06 -0500

    Unpublished blog entries are no longer displayed.

--- app/controllers/blog.php
+++ app/controllers/blog.php
@@ -5,27 +5,34 @@ ControllerTemplate::Load('main');
 
 class ControllerBlog extends ControllerMain
 {
+    public function ActionIndex()
+    {
+        Request::Initial()->Redirect('blog/entries');
+    }
+    
     public function ActionEntry()
     {
-        // echo 'Blog entry #' . $this->request->parameter('entry_id');
         $entry = Database::current()
                      ->Query('SELECT * FROM `cms_blog_entries` WHERE `slug`=\''
-                         . $this->request->parameter('slug') . '\' LIMIT 1')
+                         . $this->request->parameter('slug') . '\' AND '
+                         . '`published`=1 LIMIT 1')
                      ->Fetch();
         
-        if ($entry)
-        {
-            $this->template->Variables(array(
-                    'page_title' => $entry['title'],
-                    'content' => $entry['content'],
-                ));
-        }
+        // Entry doesn't exist or isn't published
+        if (!$entry)
+            $this->request->Redirect('blog/entries');
+        
+        $this->template->Variables(array(
+                'page_title' => $entry['title'],
+                'content' => $entry['content'],
+            ));
     }
     
     public function ActionEntries()
     {
         $entries = Database::current()
-                       ->Query('SELECT * FROM `cms_blog_entries` ORDER BY `date_created` DESC')
+                       ->Query('SELECT * FROM `cms_blog_entries` '
+                           . 'WHERE `published`=1 ORDER BY `date_created` DESC')
                        ->FetchArray();
         
         foreach ($entries as $key => $entry)
@@ -41,11 +48,6 @@ class ControllerBlog extends ControllerMain
                         'entries' => $entries,
                     )),
             ));
-    }
-    
-    public function ActionIndex()
-    {
-        Request::Initial()->Redirect('blog/entries');
     }
 }
 ?>
