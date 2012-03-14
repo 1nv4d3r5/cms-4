571287933bad4db3c1d25615aecd5a5ad875d183
Austin Burrow <atburrow@gmail.com>
2012-03-14 00:08:00 -0500

    Auth no longer uses $_COOKIE. It now uses the initial request's cookie method.

--- system/auth.php
+++ system/auth.php
@@ -16,12 +16,13 @@ class Auth
             return $this->cookie_name;
         
         // Delete old cookie (preserving value) and store value in new cookie
-        if (isset($_COOKIE[$this->cookie_name]))
+        $old_cookie = Request::Initial()->cookie($this->cookie_name);
+        
+        if ($old_cookie)
         {
             // TODO: Because setcookie also sets header information, cookies should
             // not actually be set here. To fix this, a cookie library must be
             // made that will come into play when the response is being rendered.
-            $old_cookie = $_COOKIE[$this->cookie_name];
             setcookie($this->cookie_name, '', time() - 86400, CMS::base_url());
             setcookie($cookie_name, $old_cookie, time() + 86400, CMS::base_url());
         }
@@ -86,31 +87,29 @@ class Auth
             // Does user exist?
             if ($this->user === null)
             {
-                if (isset($_COOKIE[$this->cookie_name]))
+                $current_cookie = Request::Initial()->cookie($this->cookie_name);
+                // Check auth cookie
+                if (strlen($current_cookie) == 80)
                 {
-                    // Check auth cookie
-                    if (strlen($_COOKIE[$this->cookie_name]) == 80)
-                    {
-                        $this->user = Database::current()
-                                          ->Query("SELECT * FROM `cms_auth` "
-                                              . "JOIN `cms_users` "
-                                              . "ON `cms_auth`.`user_id`=`cms_users`.`user_id` "
-                                              . "WHERE `cms_auth`.`cookie`='" 
-                                              . Database::current()->Escape($_COOKIE[$this->cookie_name])
-                                              . "' LIMIT 1")
-                                          ->Fetch();
-                        
-                        if ($this->user)
-                            // Update cookie to expire 24 hours from now
-                            setcookie($this->cookie_name, $_COOKIE[$this->cookie_name], time() + 36400, CMS::base_url());
-                        else
-                            setcookie($this->cookie_name, '', time() - 86400, CMS::base_url());
-                    }
+                    $this->user = Database::current()
+                                        ->Query("SELECT * FROM `cms_auth` "
+                                            . "JOIN `cms_users` "
+                                            . "ON `cms_auth`.`user_id`=`cms_users`.`user_id` "
+                                            . "WHERE `cms_auth`.`cookie`='" 
+                                            . Database::current()->Escape($current_cookie)
+                                            . "' LIMIT 1")
+                                        ->Fetch();
+
+                    if ($this->user)
+                        // Update cookie to expire 24 hours from now
+                        setcookie($this->cookie_name, $current_cookie, time() + 36400, CMS::base_url());
                     else
-                    {
-                        // Remove cms_auth cookie since it's invalid.
                         setcookie($this->cookie_name, '', time() - 86400, CMS::base_url());
-                    }
+                }
+                else
+                {
+                    // Remove cms_auth cookie since it's invalid.
+                    setcookie($this->cookie_name, '', time() - 86400, CMS::base_url());
                 }
             }
             
