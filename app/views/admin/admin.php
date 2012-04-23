cdd15faf9104cdc42ba7468ff8bab452e0bd22de
Austin Burrow <atburrow@gmail.com>
2012-04-22 22:53:04 -0500

    Cleaned up views/admin

--- app/views/admin/admin.php
+++ app/views/admin/admin.php
@@ -4,9 +4,12 @@
 <html>
     <head>
         <title><?php echo $site_name; ?> Administration</title>
-        <link href="http://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet" type="text/css"/>
-        <link href="http://fonts.googleapis.com/css?family=Share" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo URL::Absolute('media/css/style.css'); ?>" rel="stylesheet" type="text/css"/>
+        <link href="http://fonts.googleapis.com/css?family=Fugaz+One"
+              rel="stylesheet" type="text/css"/>
+        <link href="http://fonts.googleapis.com/css?family=Share" 
+              rel="stylesheet" type="text/css"/>
+        <link href="<?php echo URL::Absolute('media/css/style.css'); ?>" 
+              rel="stylesheet" type="text/css"/>
         <?php if (isset($head)): ?>
             <?php echo $head; ?>
         <?php endif; ?>
@@ -19,9 +22,12 @@
                         <?php echo $site_name; ?> Administration
                     </a>
                 </h1>
-                <div style="float: right; color: #FFFFFF; font-weight: bold; margin: 35px 0 0 0;">
-                    <span>Welcome, <?php echo strlen($user['first_name']) > 0 ? $user['first_name'] : $user['username'] ?></span>
-                    <a href="<?php echo URL::Absolute('admin/auth/logout'); ?>">Logout</a>
+                <div style="float: right; color: #FFFFFF; font-weight: bold; 
+                     margin: 35px 0 0 0;">
+                    <span>Welcome, <?php echo strlen($user['first_name']) > 0 ? 
+                    $user['first_name'] : $user['username'] ?></span>
+                    <a href="<?php echo URL::Absolute('admin/auth/logout'); ?>"
+                       >Logout</a>
                 </div>
             </div>
             <div class="content_wrapper">
@@ -38,10 +44,13 @@
             <div class="menu_wrapper">
                 <ul class="menu">
                     <?php if ($user['permission_manage_users']): ?>
-                        <li><a href="<?php echo URL::Absolute('admin/user'); ?>" class="menu_item">Users</a></li>
+                        <li><a href="<?php echo URL::Absolute('admin/user'); ?>"
+                               class="menu_item">Users</a></li>
                     <?php endif; ?>
-                    <li><a href="<?php echo URL::Absolute('admin/page'); ?>" class="menu_item">Pages</a></li>
-                    <li><a href="<?php echo URL::Absolute('admin/blog'); ?>" class="menu_item">Blog</a></li>
+                    <li><a href="<?php echo URL::Absolute('admin/page'); ?>"
+                           class="menu_item">Pages</a></li>
+                    <li><a href="<?php echo URL::Absolute('admin/blog'); ?>"
+                           class="menu_item">Blog</a></li>
                 </ul>
             </div>
         </div>
--- app/views/admin/auth.php
+++ app/views/admin/auth.php
@@ -4,8 +4,10 @@
 <html>
     <head>
         <title><?php echo $page_title; ?></title>
-        <link href="http://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet" type="text/css">
-        <link href="http://fonts.googleapis.com/css?family=Share" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Fugaz+One" 
+              rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Share" 
+              rel="stylesheet" type="text/css">
         <style type="text/css">
             img {
                 border: none;
@@ -62,26 +64,40 @@
                 color: #000000;
                 font-weight: bold;
                 text-shadow: #FFFFFF 0 0 2px;
-                background: #EEEEEE; /* Old browsers */
+                background: #EEEEEE;
                 border: 1px solid #EEEEEE;
-                background: -moz-linear-gradient(top, #EEEEEE 0%, #CCCCCC 100%); /* FF3.6+ */
-                background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #EEEEEE), color-stop(100%, #CCCCCC)); /* Chrome,Safari4+ */
-                background: -webkit-linear-gradient(top, #EEEEEE 0%, #CCCCCC 100%); /* Chrome10+,Safari5.1+ */
-                background: -o-linear-gradient(top, #EEEEEE 0%, #CCCCCC 100%); /* Opera 11.10+ */
-                background: -ms-linear-gradient(top, #EEEEEE 0%, #CCCCCC 100%); /* IE10+ */
-                background: linear-gradient(top, #EEEEEE 0%, #CCCCCC 100%); /* W3C */
-                filter: progid:DXImageTransform.Microsoft.gradient(startColorstr = '#EEEEEE', endColorstr = '#CCCCCC', GradientType = 0); /* IE6-9 */
+                background: -moz-linear-gradient(top, #EEEEEE 0%, #CCCCCC 100%);
+                background: -webkit-gradient(linear, left top, left bottom,
+                    color-stop(0%, #EEEEEE), color-stop(100%, #CCCCCC));
+                background: -webkit-linear-gradient(top, #EEEEEE 0%,
+                    #CCCCCC 100%); /* Chrome10+,Safari5.1+ */
+                background: -o-linear-gradient(top, #EEEEEE 0%,
+                    #CCCCCC 100%); /* Opera 11.10+ */
+                background: -ms-linear-gradient(top, #EEEEEE 0%,
+                    #CCCCCC 100%); /* IE10+ */
+                background: linear-gradient(top, #EEEEEE 0%, #CCCCCC 100%);
+                filter: progid:DXImageTransform.Microsoft.gradient(
+                    startColorstr = '#EEEEEE', endColorstr = '#CCCCCC',
+                    GradientType = 0); /* IE6-9 */
             }
 
             a.button:active {
                 background: #EEEEEE; /* Old browsers */
-                background: -moz-linear-gradient(top,  #eeeeee 0%, #dddddd 100%); /* FF3.6+ */
-                background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#eeeeee), color-stop(100%,#dddddd)); /* Chrome,Safari4+ */
-                background: -webkit-linear-gradient(top,  #eeeeee 0%,#dddddd 100%); /* Chrome10+,Safari5.1+ */
-                background: -o-linear-gradient(top,  #eeeeee 0%,#dddddd 100%); /* Opera 11.10+ */
-                background: -ms-linear-gradient(top,  #eeeeee 0%,#dddddd 100%); /* IE10+ */
-                background: linear-gradient(top,  #eeeeee 0%,#dddddd 100%); /* W3C */
-                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#eeeeee', endColorstr='#cccccc',GradientType=0 ); /* IE6-9 */
+                background: -moz-linear-gradient(top,  #eeeeee 0%,
+                    #dddddd 100%); /* FF3.6+ */
+                background: -webkit-gradient(linear, left top, left bottom,
+                    color-stop(0%,#eeeeee), color-stop(100%,#dddddd));
+                background: -webkit-linear-gradient(top,  #eeeeee 0%,
+                    #dddddd 100%); /* Chrome10+,Safari5.1+ */
+                background: -o-linear-gradient(top,  #eeeeee 0%,
+                    #dddddd 100%); /* Opera 11.10+ */
+                background: -ms-linear-gradient(top,  #eeeeee 0%,
+                    #dddddd 100%); /* IE10+ */
+                background: linear-gradient(top,  #eeeeee 0%,
+                    #dddddd 100%); /* W3C */
+                filter: progid:DXImageTransform.Microsoft.gradient(
+                    startColorstr='#eeeeee', endColorstr='#cccccc',
+                    GradientType=0 ); /* IE6-9 */
                 outline: none;
             }
 
@@ -108,18 +124,23 @@
                     <div class="auth_status"><?php echo $auth_status; ?></div>
                 <?php endif; ?>
                 <form action="<?php echo URL::Absolute('admin/auth/login'); ?>"
-                    method="post" style="margin-left: auto; margin-right: auto; width: 450px;" id="login_form">
+                    method="post" style="margin-left: auto; margin-right: auto;
+                    width: 450px;" id="login_form">
                     <div class="auth_input_wrapper">
                         <label class="auth_input_label">Username</label>
-                        <input class="auth_input" type="text" name="username" value=""/>
+                        <input class="auth_input" type="text" name="username"
+                               value=""/>
                     </div>
                     <div class="auth_input_wrapper">
                         <label class="auth_input_label">Password</label>
-                        <input class="auth_input" type="password" name="password"/>
+                        <input class="auth_input" type="password"
+                               name="password"/>
                     </div>
                     <a href="javascript:void(0);" class="button login_button" 
-                        onclick="document.getElementById('login_form').submit(); return false;">
-                        <img src="<?php echo URL::Absolute('media/img/lock.png'); ?>"/>
+                        onclick="document.getElementById('login_form').submit();
+                            return false;">
+                        <img src="<?php echo URL::Absolute(
+                                'media/img/lock.png'); ?>"/>
                         <div>Login</div>
                     </a>
                     <input type="hidden" name="action" value="login"/>
